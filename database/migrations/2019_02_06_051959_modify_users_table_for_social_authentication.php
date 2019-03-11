bd438bba59c617ad3efd16186eca1b91ac274eaa
oropon0205 <oroshiponzu0205@gmail.com>
2019-03-11 19:36:31 +0900

    modified files

--- database/migrations/2019_02_06_051959_modify_users_table_for_social_authentication.php
+++ database/migrations/2019_02_06_051959_modify_users_table_for_social_authentication.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PrepareUsersTableForSocialAuthentication extends Migration
+class ModifyUsersTableForSocialAuthentication extends Migration
 {
     /**
      * Run the migrations.
