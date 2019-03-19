eb3f83106ef03f687ba27ffd45fd2b164b4eecfa
oropon0205 <oroshiponzu0205@gmail.com>
2019-03-19 17:31:32 +0900

    modified files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,16 @@
             enable_page_level_ads: true
           });
         </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135090666-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-135090666-1');
+        </script>
     @endif
 
     <!-- Script variable -->
